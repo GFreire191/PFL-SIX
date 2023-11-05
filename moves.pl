9f74a9a859b979bbe017a3d9e845aee9a0de2241
Freirex101 <guilodinho@gmail.com>
2023-11-05 20:10:15 +0000

    Error

--- moves.pl
+++ moves.pl
@@ -2,50 +2,50 @@
 
 % Place a new disk. Add a disk to the board, at the beggining of the list located at the given row and column.
 
-place_disk(Board, Row, Column, Player, NewBoard) :-
-    nth0(Row, Board, RowList),
+place_disk(GameState, Row, Column, Player, NewGameState) :-
+    nth0(Row, GameState, RowList),
     nth0(Column, RowList, ColumnList),
     append([Player], ColumnList, NewColumnList),
     replace(RowList, Column, NewColumnList, NewRowList),
-    replace(Board, Row, NewRowList, NewBoard).
+    replace(GameState, Row, NewRowList, NewGameState).
     
 
 
 
 
-move_tower(Board,Player, OldRow, OldColumn, NewRow, NewColumn, NewBoard) :-
+move_tower(GameState,Player, OldRow, OldColumn, NewRow, NewColumn, NewGameState) :-
     
-    append_tower(Board, OldRow, OldColumn, NewRow, NewColumn, NewBoard),
+    append_tower(GameState, OldRow, OldColumn, NewRow, NewColumn, NewGameState),
     write('Tower moved!'), nl, nl.
     
 
 
-move_part_tower(Board, Player, Amount, OldRow, OldColumn, NewRow, NewColumn, NewBoard) :-
-    nth0(OldRow, Board, OldRowList),
+move_part_tower(GameState, Player, Amount, OldRow, OldColumn, NewRow, NewColumn, NewGameState) :-
+    nth0(OldRow, GameState, OldRowList),
     nth0(OldColumn, OldRowList, OldColumnList),
     length(OldColumnList, Length),
     Length >= Amount,
     take(Amount, OldColumnList, PartToMove, Remaining),
-    nth0(NewRow, Board, NewRowList),
+    nth0(NewRow, GameState, NewRowList),
     nth0(NewColumn, NewRowList, NewColumnList),
     append(PartToMove, NewColumnList, NewColumnList2),
     replace(NewRowList, NewColumn, NewColumnList2, NewRowList2),
-    replace(Board, NewRow, NewRowList2, NewBoard1),
+    replace(GameState, NewRow, NewRowList2, NewGameState1),
     replace(OldRowList, OldColumn, Remaining, NewRowList3),
-    replace(NewBoard1, OldRow, NewRowList3, NewBoard),
+    replace(NewGameState1, OldRow, NewRowList3, NewGameState),
     write('Part of tower moved!'), nl, nl.
 
 
 
-append_tower(Board, OldRow, OldColumn, NewRow, NewColumn, NewBoard) :-
-    nth0(OldRow, Board, OldRowList),
+append_tower(GameState, OldRow, OldColumn, NewRow, NewColumn, NewGameState) :-
+    nth0(OldRow, GameState, OldRowList),
 
     nth0(OldColumn, OldRowList, OldColumnList),
 
     length(OldColumnList, Length),
     Length > 0,
 
-    nth0(NewRow, Board, NewRowList),
+    nth0(NewRow, GameState, NewRowList),
 
     nth0(NewColumn, NewRowList, NewColumnList),
 
@@ -54,23 +54,23 @@ append_tower(Board, OldRow, OldColumn, NewRow, NewColumn, NewBoard) :-
 
     append(OldColumnList, NewColumnList, NewColumnList2),
     
-    replaces(Board, OldRow, OldColumn, NewRow, NewColumn, OldRowList, OldColumnList, NewRowList, NewColumnList, NewColumnList2, NewBoard).
+    replaces(GameState, OldRow, OldColumn, NewRow, NewColumn, OldRowList, OldColumnList, NewRowList, NewColumnList, NewColumnList2, NewGameState).
 
 
-replaces(Board, OldRow, OldColumn, NewRow, NewColumn, OldRowList, OldColumnList, NewRowList, NewColumnList, NewColumnList2, NewBoard) :- 
+replaces(GameState, OldRow, OldColumn, NewRow, NewColumn, OldRowList, OldColumnList, NewRowList, NewColumnList, NewColumnList2, NewGameState) :- 
 
     replace(NewRowList, NewColumn, NewColumnList2, NewRowList2),
 
-    replace(Board, NewRow, NewRowList2, NewBoard1),
+    replace(GameState, NewRow, NewRowList2, NewGameState1),
 
-    nth0(OldRow, NewBoard1, Remover),
+    nth0(OldRow, NewGameState1, Remover),
 
     nth0(OldColumn, Remover, Removido),
 
     
     replace(Remover, OldColumn, [], NewRowList3),
 
-    replace(NewBoard1, OldRow, NewRowList3, NewBoard).
+    replace(NewGameState1, OldRow, NewRowList3, NewGameState).
 
 
 
--- project.pl
+++ project.pl
@@ -14,7 +14,7 @@ clear :- write('\33\[2J').
 
 
 
-makeSix :-
+play :-
     print_menu,
     read(Option), nl,
     process_option(Option).
@@ -30,14 +30,14 @@ process_option(2) :-
     start_game(2).
 
 process_option(4) :-
-    makeSix.
+    play.
 
 process_option(5) :- 
     !, write('Goodbye!'), nl.
 
 process_option(_) :- 
     write('Invalid Option!'), nl,
-    makeSix.
+    play.
 
 % ---------------------------------------
 
@@ -45,8 +45,8 @@ start_game(1) :-
     write('What is the board Size? (4,5)'),
     read(BoardSize),
     (BoardSize == 4 ; BoardSize == 5) -> 
-        initial_GameState(BoardSize,Board),
-        game_loop(BoardSize,Board, w);
+        initial_GameState(BoardSize,GameState),
+        game_loop(BoardSize,GameState, w);
     write('Invalid board size. Please enter 4 or 5.'),
     start_game(1).
 
@@ -54,30 +54,30 @@ start_game(2) :-
     write('What is the board Size? (4,5)'),
     read(BoardSize),
     (BoardSize == 4 ; BoardSize == 5) -> 
-        initial_board(BoardSize,Board),
-        game_loop_PR(BoardSize,Board, w); %Loop of player vs Robot
+        initial_GameState(BoardSize,GameState),
+        game_loop_PR(BoardSize,GameState, w); %Loop of player vs Robot
     write('Invalid board size. Please enter 4 or 5.'),
     start_game(2).
     
 
 
 % If check matrix fails, handle matrix
-process_option_game(1, Board, Player,BoardSize, NewBoard) :-
+process_option_game(1, GameState, Player,BoardSize, NewGameState) :-
     write('Which row?'),
     read(Row),
     write('Which column?'),
     read(Column),
     check_matrix(Row, Column,BoardSize),
-    (nth0(Row, Board, RowList),
+    (nth0(Row, GameState, RowList),
     nth0(Column, RowList, ColumnList),
     length(ColumnList, Length),
     Length == 0 ->
-    place_disk(Board, Row, Column, Player, NewBoard);
-    handle_invalid_not_empty(Board, Player,BoardSize, NewBoard));
-    handle_invalid_matrix(Board, Player,BoardSize, NewBoard).
+    place_disk(GameState, Row, Column, Player, NewGameState);
+    handle_invalid_not_empty(GameState, Player,BoardSize, NewGameState));
+    handle_invalid_matrix(GameState, Player,BoardSize, NewGameState).
     
 
-process_option_game(2, Board, Player,BoardSize, NewBoard) :-
+process_option_game(2, GameState, Player,BoardSize, NewGameState) :-
     write('Choose the row of the tower you want to move:'),
     read(Row),
     write('Choose the column of the tower you want to move:'),
@@ -87,17 +87,17 @@ process_option_game(2, Board, Player,BoardSize, NewBoard) :-
     write('Choose the column where you want to move the tower:'),
     read(NewColumn),
     check_matrix(Row, Column,BoardSize),check_matrix(NewRow, NewColumn,BoardSize),
-    (nth0(Row, Board, RowList),
+    (nth0(Row, GameState, RowList),
     nth0(Column, RowList, ColumnList),
     length(ColumnList, Length),
     move_piece(Length, Row, Column, NewRow, NewColumn) ->
-    move_tower(Board,Player ,Row, Column, NewRow, NewColumn, NewBoard);
-    handle_invalid_moves(Board, Player,BoardSize, NewBoard));
-    handle_invalid_matrix(Board, Player,BoardSize, NewBoard).
+    move_tower(GameState,Player ,Row, Column, NewRow, NewColumn, NewGameState);
+    handle_invalid_moves(GameState, Player,BoardSize, NewGameState));
+    handle_invalid_matrix(GameState, Player,BoardSize, NewGameState).
     
 
 
-process_option_game(3, Board, Player,BoardSize, NewBoard) :-
+process_option_game(3, GameState, Player,BoardSize, NewGameState) :-
     write('Choose the row of the tower you want to move:'),
     read(Row),
     write('Choose the column of the tower you want to move:'),
@@ -107,21 +107,21 @@ process_option_game(3, Board, Player,BoardSize, NewBoard) :-
     write('Choose the column where you want to move the tower:'),
     read(NewColumn),
     check_matrix(Row, Column, BoardSize),check_matrix(NewRow, NewColumn,BoardSize),
-    (nth0(Row, Board, RowList),
+    (nth0(Row, GameState, RowList),
     nth0(Column, RowList, ColumnList),
     length(ColumnList, Length),
     move_piece(Length, Row, Column, NewRow, NewColumn) ->
     write('How many pieces do you want to move?'),
     read(Amount),
-    move_part_tower(Board, Player, Amount, Row, Column, NewRow, NewColumn, NewBoard);
-    handle_invalid_moves(Board, Player,BoardSize, NewBoard));
-    handle_invalid_matrix(Board, Player,BoardSize, NewBoard).
+    move_part_tower(GameState, Player, Amount, Row, Column, NewRow, NewColumn, NewGameState);
+    handle_invalid_moves(GameState, Player,BoardSize, NewGameState));
+    handle_invalid_matrix(GameState, Player,BoardSize, NewGameState).
 
 
 
-process_option_game(_,Board,Player,BoardSize,NewBoard) :- 
+process_option_game(_,GameState,Player,BoardSize,NewGameState) :- 
     write('INVALID OPTION!'), nl,
-    game_loop(BoardSize,Board, Player).
+    game_loop(BoardSize,GameState, Player).
 
 % ---------------------------------------
 
@@ -133,90 +133,90 @@ next_player(w, b).
 next_player(b, w).
 
 
-game_loop(BoardSize,Board, Player) :-
-    displayBoard(BoardSize,Board), nl, nl,
+game_loop(BoardSize,GameState, Player) :-
+    displayBoard(BoardSize,GameState), nl, nl,
     print_menu_game,
     write('Player '), write(Player), write(' turn:'), nl, nl,
     read(OptionGame), nl,nl,
-    process_option_game(OptionGame, Board, Player,BoardSize, NewBoard),
-    (check_win(NewBoard, Player) -> displayBoard(BoardSize,NewBoard), nl, nl, !;
+    process_option_game(OptionGame, GameState, Player,BoardSize, NewGameState),
+    (check_win(NewGameState, Player) -> displayBoard(BoardSize,NewGameState), nl, nl, !;
     next_player(Player, NextPlayer),
-    game_loop(BoardSize,NewBoard, NextPlayer)).
+    game_loop(BoardSize,NewGameState, NextPlayer)).
 
 
 %Loop of player vs robot, the robot chooses a random move
 
-game_loop_PR(BoardSize, Board, Player) :-
-    displayBoard(BoardSize, Board), nl, nl,
+game_loop_PR(BoardSize, GameState, Player) :-
+    displayBoard(BoardSize, GameState), nl, nl,
     (Player == w ->
         print_menu_game,
         write('Player '), write(Player), write(' turn:'), nl, nl,
         read(OptionGame), nl, nl,
-        process_option_game(OptionGame, Board, Player, BoardSize, NewBoard)
+        process_option_game(OptionGame, GameState, Player, BoardSize, NewGameState)
     ;
         write('Robot turn:'), nl, nl,
         random_between(1, 3, OptionGame),
-        robot_move(OptionGame, Board, b, BoardSize, NewBoard)
+        robot_move(OptionGame, GameState, b, BoardSize, NewGameState)
     ),
-    (check_win(NewBoard, Player) ->
-        displayBoard(BoardSize, NewBoard), nl, nl, !
+    (check_win(NewGameState, Player) ->
+        displayBoard(BoardSize, NewGameState), nl, nl, !
     ;
         next_player(Player, NextPlayer),
-        game_loop_PR(BoardSize, NewBoard, NextPlayer)
+        game_loop_PR(BoardSize, NewGameState, NextPlayer)
     ).
 
     
 %Robot move(1) usa o place disk para colocar um disco numa posicao aleatoria
-robot_move(1, Board, b,BoardSize, NewBoard) :-
+robot_move(1, GameState, b,BoardSize, NewGameState) :-
     random_between(0, BoardSize, Row),
     random_between(0, BoardSize, Column),
-    (nth0(Row, Board, RowList),
+    (nth0(Row, GameState, RowList),
     nth0(Column, RowList, ColumnList),
     length(ColumnList, Length),
     Length == 0 ->
-    place_disk(Board, Row, Column, b, NewBoard),
+    place_disk(GameState, Row, Column, b, NewGameState),
     write('Robot placed a disk in row '), write(Row), write(' and column '), write(Column), write('.'), nl;
-    robot_move(1, Board, b,BoardSize, NewBoard)).
+    robot_move(1, GameState, b,BoardSize, NewGameState)).
 
 %Robot move(2) usa o move_tower para mover uma torre para uma posicao aleatoria
-robot_move(2, Board, b,BoardSize, NewBoard) :-
+robot_move(2, GameState, b,BoardSize, NewGameState) :-
     random_between(0, BoardSize, Row),
     random_between(0, BoardSize, Column),
     random_between(0, BoardSize, NewRow),
     random_between(0, BoardSize, NewColumn),
-    (nth0(Row, Board, RowList),
+    (nth0(Row, GameState, RowList),
     nth0(Column, RowList, ColumnList),
     length(ColumnList, Length),
-    nth0(NewRow, Board, NewRowList),
+    nth0(NewRow, GameState, NewRowList),
     nth0(NewColumn, NewRowList, NewColumnList),
     length(NewColumnList, NewLength),
     NewLength \=0,
     move_piece(Length, Row, Column, NewRow, NewColumn) ->
-    move_tower(Board, b, Row, Column, NewRow, NewColumn, NewBoard),
+    move_tower(GameState, b, Row, Column, NewRow, NewColumn, NewGameState),
     write('Robot moved a tower from row '), write(Row), write(' and column '), write(Column), write(' to row '), write(NewRow), write(' and column '), write(NewColumn), write('.'), nl;
-    robot_move(2, Board, b,BoardSize, NewBoard)).
+    robot_move(2, GameState, b,BoardSize, NewGameState)).
 
 
 
 %Robot move(3) usa o move_part_tower para mover uma parte de uma torre para uma posicao aleatoria
 
-robot_move(3, Board, b,BoardSize, NewBoard) :-
+robot_move(3, GameState, b,BoardSize, NewGameState) :-
     random_between(0, BoardSize, Row),
     random_between(0, BoardSize, Column),
     random_between(0, BoardSize, NewRow),
     random_between(0, BoardSize, NewColumn),
-    (nth0(Row, Board, RowList),
+    (nth0(Row, GameState, RowList),
     nth0(Column, RowList, ColumnList),
     length(ColumnList, Length),
-    nth0(NewRow, Board, NewRowList),
+    nth0(NewRow, GameState, NewRowList),
     nth0(NewColumn, NewRowList, NewColumnList),
     length(NewColumnList, NewLength),
     NewLength \=0,
     move_piece(Length, Row, Column, NewRow, NewColumn) ->
     random_between(1, Length, Amount),
-    move_part_tower(Board, b, Amount, Row, Column, NewRow, NewColumn, NewBoard),
+    move_part_tower(GameState, b, Amount, Row, Column, NewRow, NewColumn, NewGameState),
     write('Robot moved '), write(Amount), write(' pieces of a tower from row '), write(Row), write(' and column '), write(Column), write(' to row '), write(NewRow), write(' and column '), write(NewColumn), write('.'), nl;
-    robot_move(3, Board, b,BoardSize, NewBoard)).
+    robot_move(3, GameState, b,BoardSize, NewGameState)).
 
     
     
@@ -230,9 +230,9 @@ robot_move(3, Board, b,BoardSize, NewBoard) :-
     
 
 % Check if a player has won
-check_win(Board, Player) :-
+check_win(GameState, Player) :-
     % For each row in the board
-    member(Row, Board),
+    member(Row, GameState),
     % For each column in the row
     member(Tower, Row),
     % If the length of the tower is 6 or more
@@ -252,30 +252,30 @@ check_win(Board, Player) :-
 
 
 
-handle_invalid_input(Board, Player,BoardSize, NewBoard):-
+handle_invalid_input(GameState, Player,BoardSize, NewGameState):-
     write('INVALID INPUT!'), nl,
     print_menu_game,
     read(OptionGame),nl,
-    process_option_game(OptionGame, Board, Player,BoardSize, NewBoard).
+    process_option_game(OptionGame, GameState, Player,BoardSize, NewGameState).
 
-handle_invalid_matrix(Board, Player,BoardSize, NewBoard):-
-    write('NOT IN BOARD RANGE!'), nl,
+handle_invalid_matrix(GameState, Player,BoardSize, NewGameState):-
+    write('NOT IN GameState RANGE!'), nl,
     print_menu_game,
     read(OptionGame),nl,
-    process_option_game(OptionGame, Board, Player,BoardSize, NewBoard).
+    process_option_game(OptionGame, GameState, Player,BoardSize, NewGameState).
     
 
-handle_invalid_moves(Board, Player,BoardSize, NewBoard):-
+handle_invalid_moves(GameState, Player,BoardSize, NewGameState):-
     write('INVALID MOVES!'), nl,
     print_menu_game,
     read(OptionGame),nl,
-    process_option_game(OptionGame, Board, Player,BoardSize, NewBoard).
+    process_option_game(OptionGame, GameState, Player,BoardSize, NewGameState).
 
-handle_invalid_not_empty(Board, Player,BoardSize, NewBoard):-
+handle_invalid_not_empty(GameState, Player,BoardSize, NewGameState):-
     write('NOT EMPTY!'), nl,
     print_menu_game,
     read(OptionGame),nl,
-    process_option_game(OptionGame, Board, Player,BoardSize, NewBoard).
+    process_option_game(OptionGame, GameState, Player,BoardSize, NewGameState).
 
 
 
